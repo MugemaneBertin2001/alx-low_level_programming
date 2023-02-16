dd4dfae627bf4ebc77142a6c6a15d703dae6612f
bertin <bertin.m2001@gmail.com>
2023-02-16 07:02:24 -0800

    return

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  *main - function for printing string on screen
- *return 0 - function indicating well running application
+ *Return - function indicating well running application
 */
 int main(void)
 {
