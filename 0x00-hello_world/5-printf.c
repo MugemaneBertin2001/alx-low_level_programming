994ccc19e592c7a8464d0cfc9c5063ea18e6384d
bertin <bertin.m2001@gmail.com>
2023-02-16 06:40:56 -0800

    few line s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 int main() {
-    printf("In the shimmering light of the full moon, ");
-    printf("a solitary wolf howls in the distance. ");
-    printf("Its haunting cry echoes through the dark forest, ");
-    printf("adding to the eerie atmosphere of the night. ");
-    printf("Amidst the rustling of leaves and snapping of twigs, ");
-    printf("the wolf's voice rises and falls in a mournful melody. ");
-    printf("Its primal call speaks to the wildness within us all, ");
-    printf("reminding us of the beauty and power of nature. ");
-    printf("And as the last notes fade away, ");
-    printf("the forest falls silent once more, ");
-    printf("leaving us with a sense of wonder and awe.\n");
-
-    return 0;
+printf("with proper grammar, but the outcome is a piece of art,")
+return 0;
 }
 
