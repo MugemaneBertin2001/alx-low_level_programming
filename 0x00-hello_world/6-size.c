65b81c11ac2dd1074aec3e19a8ab6fcb650788d1
bertin <bertin.m2001@gmail.com>
2023-02-16 12:08:56 -0800

    rwarly corrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of int: %ld byte(s)\n", sizeof(int));
 	printf("Size of long: %ld byte(s)\n", sizeof(long));
 	printf("Size of long long: %ld byte(s)\n", sizeof(long long));
-	return (0)
+	return (0);
 }
