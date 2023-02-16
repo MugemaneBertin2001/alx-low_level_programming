8ce57d5fee2d7f7cf372f968275a80131e249919
bertin <bertin.m2001@gmail.com>
2023-02-16 07:17:06 -0800

    now shortening message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("The size of char is %d byte(s).\n", sizeof(char));
-    printf("The size of short is %d byte(s).\n", sizeof(short));
-    printf("The size of int is %d bytes.(\n)", sizeof(int));
-    printf("The size of long is %d bytes.(\n)", sizeof(long));
-    printf("The size of long long is %d byte(s).\n", sizeof(long long));
-    printf("The size of float is %d byte(s).\n", sizeof(float));
-    printf("The size of double is %d byte(s).\n", sizeof(double));
-    printf("The size of long double is %d byte(s).\n", sizeof(long double));
+    printf("Size of char is %d byte(s).\n", sizeof(char));
+    printf("Size of short is %d byte(s).\n", sizeof(short));
+    printf("Size of int is %d bytes.(\n)", sizeof(int));
+    printf("Size of long is %d bytes.(\n)", sizeof(long));
+    printf("Size of long long:  %d byte(s).\n", sizeof(long long));
+    printf("Size of float:  %d byte(s).\n", sizeof(float));
+    printf("Size of double:  %d byte(s).\n", sizeof(double));
+    printf("Size of long double:  %d byte(s).\n", sizeof(long double));
 
     return 0;
 }
