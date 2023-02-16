d980f54b122a950410e426107fc3e1c45a27a9d5
bertin <bertin.m2001@gmail.com>
2023-02-16 07:24:41 -0800

    void for main

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main() {
+int main(void)
+{
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of a short: %d byte(s)\n", sizeof(short));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
