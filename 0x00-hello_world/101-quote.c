29f00a08bcf9edf3723f870b085993f323723faa
bertin <bertin.m2001@gmail.com>
2023-02-16 13:35:19 -0800

    file bettied

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
-*main: print out on standard out put
+*main - print out on standard out put
 *Return: Returns 0
-
 */
-int main(void) {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
