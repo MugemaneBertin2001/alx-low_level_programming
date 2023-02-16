67274b1ec41757621a805ec0c24aab2d07046b83
bertin <bertin.m2001@gmail.com>
2023-02-16 11:58:01 -0800

    error niddling

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,10 @@
 */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of a short: %d byte(s)\n", sizeof(short));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long: %d byte(s)\n", sizeof(long));
-printf("Size of a long long: %d byte(s)\n", sizeof(long long));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
-printf("Size of a double: %d byte(s)\n", sizeof(double));
-printf("Size of a long double: %d byte(s)\n", sizeof(long double));
-return (0);
+	printf("Size of char: %ld byte(s)\n", sizeof(char));
+    	printf("Size of short: %ld byte(s)\n", sizeof(short));
+    	printf("Size of int: %ld byte(s)\n", sizeof(int));
+    	printf("Size of long: %ld byte(s)\n", sizeof(long));
+    	printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+	return (0);
 }
