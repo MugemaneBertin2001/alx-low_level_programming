16b7e6b9ef4e8b656c9018c87b5de928be4e87a0
bertin <bertin.m2001@gmail.com>
2023-02-16 13:27:38 -0800

    final file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+*main: print out on standard out put
+*Return: Returns 0
+
+*/
+int main() {
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return 1;
+}
+
