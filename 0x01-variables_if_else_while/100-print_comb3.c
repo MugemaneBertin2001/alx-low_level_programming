b80051db01e81bc4509c19dfd1fc03eb18031900
bertin <bertin.m2001@gmail.com>
2023-02-17 14:56:44 -0800

    trying again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,11 @@ int main(void)
 		{
 			putchar(p);
 			putchar(d);
-			if (p != '8' && d != '9')
-			{
-				putchar(',');
-				putchar(' ');
+			if (p == '8' && d == '9')
+			{	continue;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	return (0);
