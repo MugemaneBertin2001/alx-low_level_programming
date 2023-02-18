71e4f6401685ffcd089ef788b69e3ee977902a41
bertin <bertin.m2001@gmail.com>
2023-02-18 07:34:51 -0800

    renewal

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,42 +1,6 @@
 	.file	"main.c"
 	.intel_syntax noprefix
 	.text
-	.section	.rodata
-.LC0:
-	.string	"%d + %d = %d\n"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	push	rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	mov	rbp, rsp
-	.cfi_def_cfa_register 6
-	sub	rsp, 16
-	mov	DWORD PTR -12[rbp], 42
-	mov	DWORD PTR -8[rbp], 3
-	mov	edx, DWORD PTR -12[rbp]
-	mov	eax, DWORD PTR -8[rbp]
-	add	eax, edx
-	mov	DWORD PTR -4[rbp], eax
-	mov	ecx, DWORD PTR -4[rbp]
-	mov	edx, DWORD PTR -8[rbp]
-	mov	eax, DWORD PTR -12[rbp]
-	mov	esi, eax
-	lea	rdi, .LC0[rip]
-	mov	eax, 0
-	call	printf@PLT
-	mov	eax, 0
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
