f544bf5db1b817ec96c23b99a9eff1f8d85e1f66
bertin <bertin.m2001@gmail.com>
2023-04-08 06:38:34 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,33 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "main.h"
 
-int create_file(const char *filename) {
-    // Open the file for writing, creating it if it doesn't exist
-    int fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-        perror("open");
-        return -1;
-    }
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
 
-    // Close the file and return success
-    close(fd);
-    return 0;
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
-
